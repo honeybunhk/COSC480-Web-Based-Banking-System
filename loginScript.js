9d0e4d9a7e41a3628e96be5e0a78d889c3ed0049
hanan <156007870+honeybunhk@users.noreply.github.com>
2026-05-10 10:35:29 -0400

    Add files via upload

---
+++ loginScript.js
@@ -0,0 +1,65 @@
+
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const loginBtn = document.getElementById('loginBtn');
+const statusReport = document.getElementById('msg');
+
+const loginUrl = '/loginUser';
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    loginBtn.addEventListener('click', login);
+});
+
+
+// signup function
+// function to send stuff from front to back
+async function login(e) {
+    e.preventDefault();
+    if (username.value == '' || password.value == '') return;
+
+    // fetch resources from url using method 'post'
+    const res = await fetch(loginUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            username: username.value,
+            password: password.value
+        })
+    });
+
+    // report
+    if (res.status == 300){
+
+        // replace window with the bank stuff.
+        // sort of a 'redirect' to a link.
+        // done from the client side / front end.
+        window.location.href = `http://localhost:8080/bank`
+
+        username.value = '';
+        password.value = '';
+
+    }
+    // if username is not found in db
+    else if (res.status == 404){
+        username.value = '';
+        password.value = '';
+        
+        alert('Invalid credentials / User not found!');
+        statusReport.innerHTML = 'Invalid credentials / User not found';
+        statusReport.style.color = 'red';
+    }
+    
+    // invalid password
+    else if (res.status == 403){
+        username.value = '';
+        password.value = '';
+
+        alert('Invalid password!');
+        statusReport.innerHTML = 'Invalid password';
+        statusReport.style.color = 'red';
+    }
+
+}
